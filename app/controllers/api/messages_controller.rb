e0d6f3b5764c6639b85d082be90038376d628dea
tingamapuro04 <alponcemobutu100@gmail.com>
2023-02-20 21:54:58 +0300

    api

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,6 @@
+class Api::MessagesController < ApplicationController
+  def random_message 
+    message = Message.order("RANDOM").first
+    render json: {greeting: message.greeting}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "greetings#random_message"
+
+  namespace :api do
+    get "/messages/random_message", to: "greetings#random_message"
+  end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
